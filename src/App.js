ef13faf0821f90e85e9881a3ea021600eb237450
sravaninellore <nellorevenkatasravani@gmail.com>
2023-06-25 16:11:44 +0530

    commit message

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { useState } from 'react';
 import './App.css';
-
+import InputEmoji from 'react-input-emoji';
 
 const user_list = ["Alan", "Bob", "Carol", "Dean", "Elin"];
-const emojis = ["😀", "😃", "😄", "😊", "😎", "😍", "🥳", "🤩", "😘", "🤗"];
 
 const App = () => {
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const [selectedEmoji, setSelectedEmoji] = useState('');
 
-  const handleInputChange = (e) => {
-    setMessage(e.target.value);
+  const handleInputChange = (text) => {
+    setMessage(text);
   };
 
   const handleSendMessage = () => {
@@ -20,8 +18,9 @@ const App = () => {
       const newMessage = {
         user: randomUser,
         text: message,
-        likes: 0
-      };
+        likes: 0,
+        timestamp: new Date() // Store the current date and time
+        };
       setMessages([...messages, newMessage]);
       setMessage('');
     }
@@ -33,105 +32,89 @@ const App = () => {
     setMessages(updatedMessages);
   };
 
-  const handleEmojiSelect = (emoji) => {
-    setSelectedEmoji(emoji);
-  };
-
-  const handleKeyDown = (e) => {
-    if (e.key === 'Enter') {
-      handleSendMessage();
-    }
+  const handleOnEnter = (text) => {
+    setMessage(text);
+    handleSendMessage();
   };
 
   return (
     <div className="chat-container">
-      <div className="left-column">
-        <div className="chat-item">
-          <div className="circle">
-            <span>+</span>
-          </div>
-          <div className="chat-info">
-            <div className="chat-name">Conversations</div>
-          </div>
+    <div className="left-column">
+      <div className="chat-item-3">
+        <div className="circle">
+          <span>RR</span>
         </div>
-        <div className="chat-item">
-          <div className="square">#</div>
-          <div className="chat-info">
-            <div className="chat-name">Poland Office</div>
-          </div>
-        </div>
-        <div className="chat-item1">
-          <div className="square highlighted">#</div>
-          <div className="chat-info">
-            <div className="chat-name-highlighted">Introductions</div>
-            <div className="chat-description"></div>
-          </div>
-        </div>
-        <div className="chat-item">
-          <div className="square">#</div>
-          <div className="chat-info">
-            <div className="chat-name">India Office</div>
-          </div>
+        <div className="chat-info">
+          <div className="chat-name">Ronalde Raimondi</div>
+          <div className="chat-description">Research Nurse</div>
         </div>
       </div>
-      <div className="right-column">
-        <div className="chat-header">
-          <div className="chat-info">
-            <div className="chat-name">Introductions</div>
-            <div className="chat-description">This Channel Is For Company Wide Chatter</div>
-          </div>
-          <div className="contact-info">
-            <div className="contact-count">3</div>
-            <div className="contact-symbol">|</div>
-            <div className="contact-total">100</div>
-            <span className="groups-symbol">&#x1F465;</span>
-          </div>
+      <div className="chat-item-2">
+      <div className="chat-name-1">Conversations</div>
+      <div className="chat-info">
+          <div className="circle-1">+</div>
         </div>
-        <div className="messages-container">
-          <div className="message-history">
-            {messages.map((msg, index) => (
-              <div className="message" key={index}>
-                <div className="user">{msg.user}</div>
-                <div className="text">{msg.text}</div>
-                <div className="likes">
-                  <button className="like-button" onClick={() => handleLikeMessage(index)}>
-                  &#x2764; ({msg.likes})
-                  </button>
-                </div>
+      </div>
+      <div className="chat-item-4">
+        <div className="square">#</div>
+        <div className="chat-info">
+          <div className="chat-name">Poland Office</div>
+        </div>
+      </div>
+      <div className="chat-item-1">
+        <div className="square highlighted">#</div>
+        <div className="chat-info">
+          <div className="chat-name-highlighted">Introductions</div>
+          <div className="chat-description"></div>
+        </div>
+      </div>
+      <div className="chat-item-5">
+        <div className="square">#</div>
+        <div className="chat-info">
+          <div className="chat-name">India Office</div>
+        </div>
+      </div>
+    </div>
+    <div className="right-column">
+      <div className="chat-header">
+        <div className="chat-info">
+          <div className="chat-name">Introductions</div>
+          <div className="chat-description">This Channel Is For Company Wide Chatter</div>
+        </div>
+        <div className="contact-info">
+          <div className="contact-count">3</div>
+          <div className="contact-symbol">|</div>
+          <div className="contact-total">100</div>
+          <span className="groups-symbol">&#x1F465;</span>
+        </div>
+      </div>
+      <div className="messages-container">
+        <div className="message-history">
+          {messages.map((msg, index) => (
+            <div className="message" key={index}>
+              <div className="user">{msg.user}</div>
+              <div className="timestamp">{msg.timestamp.toLocaleTimeString()}</div>
+              <div className="text">{msg.text}</div>
+              <div className="likes">
+                <button className="like-button" onClick={() => handleLikeMessage(index)}>
+                &#x2764; ({msg.likes})
+                </button>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
-        <div className="input-container">
-          <input
-            type="text"
-            placeholder="Type a message..."
+      </div>
+      <div className="input-container">
+          <InputEmoji
             value={message}
             onChange={handleInputChange}
-            onKeyDown={handleKeyDown}
+            cleanOnEnter
+            onEnter={handleOnEnter}
+            placeholder="Type a message..."
           />
-          <div className="emoji-dropdown">
-            <button className="emoji-button" onClick={() => handleEmojiSelect(selectedEmoji)}>
-              {selectedEmoji ? selectedEmoji : "😊"}
-            </button>
-            {selectedEmoji && (
-              <div className="emoji-menu">
-                {emojis.map((emoji, index) => (
-                  <div
-                    className="emoji-option"
-                    key={index}
-                    onClick={() => handleEmojiSelect(emoji)}
-                  >
-                    {emoji}
-                  </div>
-                ))}
-              </div>
-            )}
-          </div>
         </div>
       </div>
     </div>
   );
 };
-
 export default App;
